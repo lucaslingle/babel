d083dc7888dc5679b88fef7e93c905d87881d7da
lucaslingle <lucasdaxlingle@gmail.com>
2025-06-09 20:27:33 -0700

    added a config for debugging

---
+++ src/babel/configs/gradpower_foom.py
@@ -0,0 +1,24 @@
+from babel.configs.base import get_base_config
+
+
+def get_config():
+    config = get_base_config()
+
+    # mesh
+    config.n_mesh_rows = 32
+    config.n_mesh_cols = 4
+
+    # width
+    config.d_model = 2304
+    config.n_layer = 18
+
+    # optimization
+    config.tokens_per_global_batch = 2 ** 18
+    config.grad_power = 0.1
+    config.optim_name = "adamw"
+
+    # periodic action settings
+    config.n_pretrain_step = 100_000
+    config.n_warmup_step = 2_000
+
+    return config
