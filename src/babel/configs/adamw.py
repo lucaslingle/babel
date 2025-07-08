fd076bb9bbb20203bc96d9a91a52cd7607ead467
lucaslingle <lucasdaxlingle@gmail.com>
2025-07-08 13:06:24 -0700

    update config and project name

--- src/babel/configs/adamw.py
+++ src/babel/configs/adamw.py
@@ -9,8 +9,8 @@ def get_config():
     config.optim_name = "adamw"
     config.dataset_name = "fineweb"
     config.model_size = 13 * (128 ** 2) * (28 ** 3)
-    config.token_budget = (2 ** 21) * 50_000
-    config.tokens_per_global_batch = 2 ** 21
+    config.token_budget = (2 ** 19) * 200_000
+    config.tokens_per_global_batch = 2 ** 19
     config.wd_lam = 0.00001
     config.wd_indep = True
     config.qk_norm = True
